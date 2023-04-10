d7e773d9a3e8d6a2ac3bf609d037ea290c1245c3
Prem MANN <premmannpnc@gmail.com>
2023-04-10 09:29:02 +0700

    modified in Navigation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="relative w-full h-screen mx-auto">
+      <div className={`${styles.paddingX} absolute inset-0
+      top-[120px] max-w-7xl mx-auto flex flex-row items-start grap-5`}>
+        <div className=" flex flex-col justify-center items-center mt-5">
+            <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+            <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi I am <span className="text-[#915eff]">GG</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            Hi there, my name is Prem MANN and I'm a software developer with 
+            experience in web application development and database management. 
+            I'm currently working as a React Developer at Krawma & Associates, 
+            where I specialize in billing systems development and optimizing 
+            applications for maximum speed and scalability. My expertise in 
+            front-end development includes proficiency in technologies such as 
+            JavaScript, React.js, Tailwind CSS, and SASS. In addition, 
+            I have experience working with PHP and React Native.
+          </p>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
   const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className={`${styles.paddingX} w-ful flex item-center py-5 top-0 z-20 bg-primary`}>
         {/* <div className='w-full justify-between item-center max-w-7x1 max-auto'> */}
@@ -19,7 +20,10 @@ const Navbar = () => {
               window.scrollTo(0, 0);
             }}>
             <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
-            <p className='text-white text-[18px] font-bold cursor-pointer'> GG<span className='sm:block hidden'>| PREM</span></p>
+            <p className='text-white text-[18px] font-bold cursor-pointer flex'>
+               Rathana &nbsp;
+               <span className='sm:block hidden'>| PREM</span>
+            </p>
           </Link>
 
           <ul className='list-none hidden sm:flex flex-row gap-10'>
@@ -30,6 +34,31 @@ const Navbar = () => {
                 </li>
               ))}
           </ul>
+
+          <div className="sm:hidden flex flex-1 justify-end items-center">
+                <img 
+                src={toggle ?  close : menu}
+                alt="menu"
+                className="w-[28px] h-[28px] object-contain cursor-pointer"
+                onClick={() => setToggle(!toggle)}
+                />
+                <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0
+                mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+                    <ul className='list-none flex justify-end items-start flex-col gap-4'>
+                        {navLinks.map((link) =>(
+                          <li key={link.id}
+                              className={`${active === link.title ? "text-white" : "text-secondary"} 
+                              font-poppins font-medium cursor-pointer text-[16px]`}
+                              onClick={() => {
+                                setActive(!toggle);
+                                setActive(link.title);
+                              }}>
+                            <a href={`#${link.id}`}>{link.title}</a>
+                          </li>
+                        ))}
+                    </ul>
+                </div>
+          </div>
           
         </div>
     </nav>
